62cb72c535095cfce05b6ec3b6de0a8c1682c081
RBeard157 <149635562+RBeard157@users.noreply.github.com>
2025-11-07 16:55:14 -0600

    Tutor.js

---
+++ javascript/Tutor.js
@@ -0,0 +1,63 @@
+const User = require('./User');
+const db = require('../db');
+
+class Tutor extends User {
+    constructor(firstName, surName, samID, refID = null) {
+        super(firstName, surName, samID, 'Tutor', refID);
+    }
+
+    // addAvailability: adds a new time slot
+    async addAvailability(classNo, timeSlot) {
+        try {
+            const sql = `
+                INSERT INTO Avail (TutorRefNo, ClassNo, TimeSlot, IsBooked)
+                VALUES (?, ?, ?, false)
+            `;
+            await db.execute(sql, [this.refID, classNo, timeSlot]);
+            console.log("Availability added:", classNo, timeSlot);
+            return true;
+        } catch (err) {
+            console.error("Error in addAvailability:", err.message);
+            return false;
+        }
+    }
+
+    // removeAvailability: deletes a time slot
+    async removeAvailability(availId) {
+        try {
+            const sql = 'DELETE FROM Avail WHERE AvailID = ? AND TutorRefNo = ?';
+            const [result] = await db.execute(sql, [availId, this.refID]);
+
+            if (result.affectedRows > 0) {
+                console.log("Availability removed:", availId);
+                return true;
+            } else {
+                console.log("No availability found to remove");
+                return false;
+            }
+        } catch (err) {
+            console.error("Error in removeAvailability:", err.message);
+            return false;
+        }
+    }
+
+    // viewBooking: shows tutor's availability schedule
+    async viewBooking() {
+        try {
+            const sql = `
+                SELECT AvailID, ClassNo, TimeSlot, IsBooked
+                FROM Avail
+                WHERE TutorRefNo = ?
+                ORDER BY TimeSlot ASC
+            `;
+            const [rows] = await db.execute(sql, [this.refID]);
+            console.log("Availability for", this.firstName, rows);
+            return rows;
+        } catch (err) {
+            console.error("Error in viewBooking:", err.message);
+            return [];
+        }
+    }
+}
+
+module.exports = Tutor;

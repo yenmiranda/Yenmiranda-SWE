921d7dac054144271dda0ea6a3db2c95d83dfde6
Justin Hardin <jlhardin03@proton.me>
2025-11-10 02:20:35 -0600

    Update server.js

--- javascript/server.js
+++ javascript/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2/promise');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const db = require('./db')
 const fs = require('fs'); 
